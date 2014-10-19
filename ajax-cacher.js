9947cd43af807c8776138fe91456935e57b13c1b
Omar Meky <omar.meky@udu.nu>
2014-10-18 22:37:58 -0400

    initial commit

---
+++ ajax-cacher.js
@@ -0,0 +1,33 @@
+window.ajaxCacher = function(){
+    this.get = function(url, cb){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState == 4){
+                if (xhr.status == 200){
+                    cb({
+                        data: xhr.responseText
+                    });
+                    localStorage.setItem(url, JSON.stringify({
+                        data: xhr.responseText,
+                        timestamp: new Date()
+                    }));
+                }
+                else if (xhr.status == 304){
+                    cb({
+                        data: JSON.parse(localStorage.getItem('url')).data
+                    });
+                }
+                else{
+                    cb({
+                        error: "Unable to retrieve data"
+                    });
+                }
+            }
+        };
+        xhr.open('GET', url, true);
+        if (url in localStorage){
+            xhr.setRequestHeader('If-Modified-Since', JSON.parse(localStorage.getItem('url')).timestamp);
+        }
+        xhr.send();
+    };
+};
